c18822fc2085c432bef73d632803fe1f5e040813
KennedyMungai <kennedymungaifmab@gmail.com>
2022-06-05 19:27:41 +0300

    Migration files

--- IdentityAgain/Data/Migrations/20220605162636_CreateIdentitySchemaAnother.Designer.cs
+++ IdentityAgain/Data/Migrations/20220605162636_CreateIdentitySchemaAnother.Designer.cs
@@ -4,6 +4,7 @@ using IdentityAgain.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace IdentityAgain.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220605162636_CreateIdentitySchemaAnother")]
+    partial class CreateIdentitySchemaAnother
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ IdentityAgain/Data/Migrations/20220605162636_CreateIdentitySchemaAnother.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IdentityAgain.Data.Migrations
+{
+    public partial class CreateIdentitySchemaAnother : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
